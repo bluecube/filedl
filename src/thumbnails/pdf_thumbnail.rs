29a89766f1ebca18d71e141a74f22c235233a4e9
Kuba Marek <blue.cube@seznam.cz>
2026-02-17 15:14:41 +0100

    PDF thumbnails always cover whole page, flattening alpha to white background

--- src/thumbnails/pdf_thumbnail.rs
+++ src/thumbnails/pdf_thumbnail.rs
@@ -1,12 +1,9 @@
 use std::{fs::read, path::Path, sync::Arc};
 
 use hayro::{RenderSettings, hayro_interpret::InterpreterSettings, hayro_syntax::Pdf};
-use image::{GenericImageView as _, RgbaImage};
+use image::{Rgba, RgbaImage};
 
-use crate::{
-    error::{FiledlError, Result},
-    thumbnails::cropping::crop_coordinates,
-};
+use crate::error::{FiledlError, Result};
 
 pub fn create_pdf_thumbnail(file: &Path, resolution: (u32, u32)) -> Result<RgbaImage> {
     let file_data = Arc::new(read(file)?);
@@ -35,25 +32,46 @@ pub fn create_pdf_thumbnail(file: &Path, resolution: (u32, u32)) -> Result<RgbaI
     let rendered_w = pixmap.width() as u32;
     let rendered_h = pixmap.height() as u32;
 
-    let subpixels = {
-        let mut subpixels = Vec::with_capacity(rendered_w as usize * rendered_h as usize * 4);
+    assert!(rendered_w <= resolution.0);
+    assert!(rendered_h <= resolution.1);
 
-        subpixels.extend(pixmap.take().into_iter().flat_map(|premul_rgba| {
-            // TODO: Un-premultiply
-            premul_rgba.to_u8_array().into_iter()
-        }));
+    let converted_pixmap_pixel = |x: u32, y: u32| -> Rgba<u8> {
+        let px = pixmap.sample(x as u16, y as u16);
 
-        subpixels
+        // Flatten alpha channel onto white background
+        let convert = |v| v + (255 - px.a) as u8;
+        Rgba([convert(px.r), convert(px.g), convert(px.b), 255])
+
+        // Un-premultiply alpha, keep transparency
+        // if px.a == 0 {
+        //     Rgba([px.r, px.g, px.b, px.a])
+        // } else {
+        //     let convert = |v| ((v as u16) * 255u16 / (px.a as u16)) as u8;
+        //     Rgba([convert(px.r), convert(px.g), convert(px.b), px.a])
+        // }
     };
 
-    let image = RgbaImage::from_raw(rendered_w, rendered_h, subpixels).unwrap();
+    let image = if rendered_w < resolution.0 {
+        let min_x = (resolution.0 - rendered_w) / 2;
+        let max_x = min_x + rendered_w;
+        RgbaImage::from_fn(resolution.0, resolution.1, |x, y| {
+            if x < min_x || x >= max_x {
+                Rgba([0, 0, 0, 0])
+            } else {
+                converted_pixmap_pixel(x - min_x, y)
+            }
+        })
+    } else {
+        let min_y = (resolution.1 - rendered_h) / 2;
+        let max_y = min_y + rendered_h;
+        RgbaImage::from_fn(resolution.0, resolution.1, |x, y| {
+            if y < min_y || y >= max_y {
+                Rgba([0, 0, 0, 0])
+            } else {
+                converted_pixmap_pixel(x, y - min_y)
+            }
+        })
+    };
 
-    let (crop_x, crop_y, crop_w, crop_h) = crop_coordinates(
-        (rendered_w, rendered_h),
-        resolution,
-        0, /* Top of the page is most important */
-    );
-
-    let view = image.view(crop_x, crop_y, crop_w, crop_h);
-    Ok(view.to_image())
+    Ok(image)
 }
